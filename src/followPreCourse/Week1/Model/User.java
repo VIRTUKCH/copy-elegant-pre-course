db3459ba58f3b8e26395bb0fa182c531d53550f1
kch4731 <kch4731@naver.com>
2022-10-03 18:39:16 +0900

    refactor: Passed method to Controller from User
    User 클래스에서 Controller 클래스로 메서드를 넘겼습니다.
    Model을 담당하는 클래스에서는 가능하면 멤버변수를 연산하는 일이 없어야 합니다.
    Controller을 통해서 연산하고, View에게 넘겨주는 형식을 추구해야 합니다.
    
    [StudyAboutMVCPattern의 일부]
    1. Model은 Controller와 View에 의존해서는 안된다.
    : Model 내부에 Controller와 View에 관련된 코드가 있으면 안된다는 소리다.
    가장 쉽게 알 수 있는 흔적은 Model이 들어있는 package에 Controller나 View가 import 되었는지를 보면 되겠다.
    
    를 보고 작성했습니다.

--- src/followPreCourse/Week1/Model/User.java
+++ src/followPreCourse/Week1/Model/User.java
@@ -1,31 +1,20 @@
+/*
+ * Model과 MVC패턴
+ * 1. Model은 Controller와 View에 의존해서는 안된다. - import 금지
+ * 2. View는 Model에만 의존해야 하며, Controller에는 의존하면 안된다.
+ * 3. View가 Model로부터 데이터를 받을 때에는, 사용자마다 다르게 보여주어야 하는 데이터만 받아야 한다.
+ * 4. View가 Model로부터 데이터를 받을 때, 반드시 Controller에서 받아야 한다.
+ */
 package followPreCourse.Week1.Model;
 
-import java.util.Scanner;
-
-import followPreCourse.Week1.View.UI;
-
 public class User {
-	private int guessNumberOfComputer;
+	private int guessingNumber;
+	public User(int guessingNumber) {
+		this.guessingNumber = guessingNumber;
+		System.out.println("시험 출력 : " + this.guessingNumber);
+	}
 	public User() {}
-	public void setGuessNumberOfComputer(int guessNumberOfComputer) {
-		this.guessNumberOfComputer = guessNumberOfComputer;
-	}
-	public void setGuessNumberOfComputer() {
-		Scanner sc = new Scanner(System.in);
-		this.guessNumberOfComputer = sc.nextInt();
-		while(!isInputValid(this.guessNumberOfComputer)) {
-			this.guessNumberOfComputer = sc.nextInt();
-		}
-		sc.close();
-		
-		System.out.println(); // 확인차 만들어 봄
-		System.out.println("<확인차 출력> - User의 입력값 : " + guessNumberOfComputer);
-	}
-	private boolean isInputValid(int nextInt) {
-		if(nextInt < 100 || nextInt > 999) {
-			UI.inputIsNotValidMent();
-			return false;
-		}
-		return true;
+	public void setGuessNumberOfComputer(int guessingNumber) {
+		this.guessingNumber = guessingNumber;
 	}
 }
